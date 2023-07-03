d89691fd2231094eae36b56d6b7046a0e72af542
l00420753 <lanxueyuan@huawei.com>
2023-07-03 10:26:51 +0800

    add ncc

--- src/hello.ts
+++ src/hello.ts
@@ -1,4 +1,5 @@
 import * as core from "@actions/core"
+import * as constants from "./constants"
 
 try {
     const whoToGreet = core.getInput("who-to-greet", {required: true})
@@ -6,6 +7,7 @@ try {
     const time = (new Date).toTimeString()
     core.setOutput("time", time)
     core.info("Hello Time " + time)
+    core.info("Hello： " + constants.INPUT_JAVA_PACKAGE)
 } catch (error) {
     core.error((error as any).message)
 }
